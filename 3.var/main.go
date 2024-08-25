7030e7b04e27478760f35588934cc9a47a42fc90
Sado4 <derasado04@gmail.com>
2024-08-25 13:04:32 +0900

    feat: var

--- 3.var/main.go
+++ 3.var/main.go
@@ -1,42 +1,51 @@
 package main
 
-import "fmt"
-
-var i int = 100
-var s string = "Golang"
-
-var t, f bool = true, false
-
-var (
-	ii int    = 1000
-	ss string = "Go"
+import (
+	"fmt"
 )
 
-var i2 int
+var i5 int = 10
 
-var sss string
-
-//i3 := 100
+func outer(o int) {
+	// var o int = 20
+	fmt.Println(o)
+}
 
 func main() {
-	fmt.Println(i2)
-	i2 = 150
-	fmt.Println(i2)
+	var num int = 10
+	fmt.Println(num)
 
-	fmt.Println(sss)
-	sss = "Go!!"
-	fmt.Println(sss)
+	var str string = "Hello"
+	fmt.Println(str)
 
-	i2 = 200
-	fmt.Println(i2)
+	var bo bool = true
+	fmt.Println(bo)
 
-	i3 := 200
-	fmt.Println(i3)
+	var num2, str2 = 20, "World"
+	fmt.Println(num2, str2)
 
-	//i3 := 300
+	var (
+		num3 int    = 30
+		str3 string = "Golang"
+	)
+	fmt.Println(num3, str3)
 
-	i3 = 300
+	var num4 int
+	var str4 string
+	var bo2 bool
+	fmt.Println(num4, str4, bo2)
 
-	//i3 = "string"
+	num4 = 40
+	str4 = "Hello"
+	bo2 = true
+	fmt.Println(num4, str4, bo2)
 
+	test := "World"
+	test2 := 50
+	test3 := true
+	fmt.Println(test, test2, test3)
+
+	fmt.Println(i5)
+
+	outer(46)
 }
