558878c527ed9300bbcdc76771f604a1af6c21a8
Sado4 <derasado04@gmail.com>
2024-08-26 15:14:20 +0900

    fix: int型

--- 4.basetype/2.main.go
+++ 4.basetype/2.main.go
@@ -7,11 +7,13 @@ func main() {
 	var i int = 100
 
 	fmt.Println(i + 50)
+	fmt.Printf("%T\n", i)
 
 	var ii int64 = 50
+	fmt.Printf("%T\n", ii)
 
 	//fmt.Println(i + ii)
 
 	fmt.Println(int32(ii))
-	fmt.Printf("%T\n", int32(ii))
+	fmt.Printf("%T\n", int(ii))
 }
